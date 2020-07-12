a9001f24302617a797949a688fa5cf76fb452f44
Joseph cohen <jojocohen57@gmail.com>
2020-07-12 20:06:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,9 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :brand, attr_accessor :color,
-  attr_accessor :size, attr_accessor :material,
-  attr_accessor :condition, attr_reader :name
+  attr_accessor :brand,  :color,
+   :size, :material,
+   :condition,
+   attr_reader :name
   
   def initialize(name)
     @name = name
